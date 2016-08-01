c2966e4d3f498086fe3f4d83ada41df61efccad5
James Pritz <james@jamespritz.com>
2016-08-01 13:28:36 -0400

    consolodate persist

---
+++ ShortBus/Publisher/IPublisher.cs
@@ -0,0 +1,31 @@
+﻿using ShortBus.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortBus.Publish {
+
+
+
+    //public interface IPublisher : IEndPoint {
+
+
+
+    //    / <summary>
+    //    / Called by the Distributor to let the bus know to send messages its way.
+    //    / if the publisher is down (ServiceIsDown), should return a servicedown exception.
+    //    / The Bus is responsible for handling, and should call ServiceIsDown first.
+    //    / </summary>
+    //    / <param name = "message" ></ param >
+    //    / < returns ></ returns >
+    //    EndpointResponse Subscribe(PersistedMessage message);
+
+    //    EndpointResponse UnSubscribe(PersistedMessage message);
+
+
+
+
+    //}
+}
---
+++ ShortBus/Publisher/SubscriptionRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortBus.Publish {
+
+
+    public class EndpointRegistrationRequest {
+        public string EndPoint { get; set; }
+        public string GUID { get; set; }
+        public string Name { get; set; }
+        public bool DeRegister { get; set; }
+    }
+
+    public class SubscriptionRequest: EndpointRegistrationRequest {
+
+        public string MessageTypeName { get; set; }
+        public bool DiscardIfSubscriberIsDown { get; set; }
+
+
+    }
+
+
+}
